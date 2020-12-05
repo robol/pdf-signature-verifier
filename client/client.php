e2a0af7151906566a25c87fbb4144bb00e6769f1
Leonardo Robol <leo@robol.it>
2020-12-05 15:28:53 +0100

    Allow arbitrary URL in client.php.

--- client/client.php
+++ client/client.php
@@ -1,10 +1,19 @@
 <?php
-  $fc = file_get_contents($argv[1]);
+  if (count($argv) > 2) {
+    $url = $argv[1];
+    $file = $argv[2];
+  }
+  else {
+    $url = "http://127.0.0.1:8081/validate";
+    $file = $argv[1];
+  }
+
+  $fc = file_get_contents($file);
   $dd = base64_encode($fc);
 
   $v = [ 'data' => $dd ];
 
-  $curl = curl_init('http://127.0.0.1:8081/validate');
+  $curl = curl_init($url);
 
   curl_setopt($curl, CURLOPT_POST, true);
   curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($v));
